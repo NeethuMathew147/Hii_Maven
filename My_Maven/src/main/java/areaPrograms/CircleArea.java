20bb2781b1b48074d09ddd65de41d8fa7acd30b2
Neethu <Neethu@LAPTOP-R8SRJLGK>
2023-02-23 15:03:41 +0400

    Area of shapes:Commit 1

---
+++ My_Maven/src/main/java/areaPrograms/CircleArea.java
@@ -0,0 +1,13 @@
+package areaPrograms;
+
+public class CircleArea {
+	public static void main(String arg[]) {
+		
+		int r=2;
+		double a= 3.14*r*r;
+		System.out.println("Area of circle is "+a);
+		
+		
+	}
+
+}
---
+++ My_Maven/src/main/java/areaPrograms/SquareArea.java
@@ -0,0 +1,13 @@
+package areaPrograms;
+
+public class SquareArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int s= a*a;
+		System.out.println("Area of square "+s);
+
+	}
+
+}
