5ecddd7cffb890bb3938fa6a95e1dadc3367a05a
Neethu <Neethu@LAPTOP-R8SRJLGK>
2023-02-23 11:03:13 +0400

    2nd Program to add 2 numbers

---
+++ My_Maven/src/main/java/basicPrograms/AddTwoNumb.java
@@ -0,0 +1,12 @@
+package basicPrograms;
+
+public class AddTwoNumb {
+	
+	public static void main(String arg[]) {
+		int a=5;
+		int b=5;
+		int c= a+b;
+		System.out.println("Sum is " + c);
+	}
+
+}
